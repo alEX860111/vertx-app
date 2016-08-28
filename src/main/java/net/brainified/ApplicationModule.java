cd0762c202def152b0657f55762af1928599bb7c
Alexander Buhr <alex86011119@googlemail.com>
2016-08-28 20:06:39 +0200

    refactored

--- src/main/java/net/brainified/ApplicationModule.java
+++ src/main/java/net/brainified/ApplicationModule.java
@@ -8,8 +8,8 @@ final class ApplicationModule extends AbstractModule {
 	@Override
 	protected void configure() {
 	  final Vertx vertx = Vertx.vertx();
+	  bind(Vertx.class).toInstance(vertx);
 		bind(ProductService.class).to(ProductServiceImpl.class);
 		bind(ProductHandler.class).to(ProductHandlerImpl.class);
-		bind(Vertx.class).toInstance(vertx);
 	}
 }
